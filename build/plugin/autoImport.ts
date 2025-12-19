1457729a99174678d395aeb220bf9dbde9abc83b
ZuoJiangtao <1799158837@qq.com>
2025-12-19 09:36:24 +0800

    feat(vite): autoImport插件配置更改

--- build/plugin/autoImport.ts
+++ build/plugin/autoImport.ts
@@ -6,26 +6,39 @@ export function configPluginAutoImport(): PluginOption | PluginOption[] {
   return AutoImport({
     imports: [
       'vue',
-      'vue-router',
+      // 'vue-router',
       {
         '@vueuse/core': [
           // named imports
-          'useMouse', // import { useMouse } from '@vueuse/core',
-          'useTitle',
+          // 'useMouse', // import { useMouse } from '@vueuse/core',
+          // 'useTitle',
           'useDebounceFn',
           // alias
           // ['useFetch', 'useMyFetch'], // import { useFetch as useMyFetch } from '@vueuse/core',
         ],
-        axios: [
-          // default imports
-          ['default', 'axios'], // import { default as axios } from 'axios',
-        ],
+        // axios: [
+        //   // default imports
+        //   ['default', 'axios'], // import { default as axios } from 'axios',
+        // ],
       },
       'pinia',
+      'uni-app',
+      {
+        from: 'uni-mini-router',
+        imports: ['createRouter', 'useRouter', 'useRoute'],
+      },
+      {
+        from: 'wot-design-uni',
+        imports: ['useToast', 'useMessage', 'useNotify', 'CommonUtil'],
+      },
+      {
+        from: 'alova/client',
+        imports: ['usePagination', 'useRequest'],
+      },
     ],
     // resolvers: [AntDesignVueResolver()],
     vueTemplate: true,
-    dirs: ['src/hooks', 'src/components'],
+    dirs: ['src/hooks', 'src/store', 'src/utils', 'src/api'],
     dts: 'types/auto-imports.d.ts',
   });
 }
