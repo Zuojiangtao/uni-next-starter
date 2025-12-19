e3dea2555bdaa1129960701fe08a56e562d34cf6
ZuoJiangtao <1799158837@qq.com>
2025-12-19 09:30:16 +0800

    feat(router): 路由配置

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createSSRApp } from 'vue'
 import App from './App.vue'
 
 import store from './store'
+import router from './router'
 import 'virtual:uno.css'
 
 export function createApp() {
   const app = createSSRApp(App)
   app.use(store)
+  app.use(router)
 
   return {
     app,
