91ca2010482b457badec849952eed6bbdfb1c1b0
ZuoJiangtao <1799158837@qq.com>
2025-12-19 09:33:21 +0800

    feat(root): 虚拟根组件

--- src/App.ku.vue
+++ src/App.ku.vue
@@ -5,5 +5,14 @@
 <template>
   <wd-config-provider :theme-vars="themeVars" :theme="theme" :custom-class="`page-wraper ${theme}`">
     <ku-root-view />
+    <wd-notify />
+    <wd-message-box />
+    <wd-toast />
+    <global-loading />
+    <global-toast />
+    <global-message />
+    <!-- #ifdef MP-WEIXIN -->
+    <privacy-popup />
+    <!-- #endif -->
   </wd-config-provider>
 </template>
